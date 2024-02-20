db94ab1b08ccc2a6f1aa98863b76ad9a4333e911
pond7897 <80212418+pond7897@users.noreply.github.com>
2024-02-20 08:02:59 +0000

    ttt

--- python/inclass/python_super/Person.py
+++ python/inclass/python_super/Person.py
@@ -2,6 +2,8 @@ class Person:
     def __init__(self,name,age):
         self.name = name
         self.age = int(age)
+    def __str__ (self):
+        return 'name: {0}\nage: {1}'.format(self.name,self.age)
 
 class Student(Person):
     def __init__(self,name,age,grades):
@@ -9,6 +11,6 @@ class Student(Person):
         self.grades = float(grades)
 
     def __str__ (self):
-        return f'name: {self.name}\nage: {self.age}\ngrade: {self.grades}'
-student = Student('Titipong',20,'4.00')           
+        return super().__str__() +'\ngrade: {:.2f}'.format(self.grades)
+student = Student('Thanida', 20, 4.00)           
 print(student)
