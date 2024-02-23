08f9a072b791ebbbd930f5e6df171e3e61c73dd1
pond7897 <80212418+pond7897@users.noreply.github.com>
2024-02-24 02:47:55 +0700

    commit

---
+++ python/Homework/InchtoCenti.py
@@ -0,0 +1,7 @@
+def inchtoCenti(inch):
+    centi = inch * 2.54
+    return centi
+def main():
+    inch = int(input("Enter inch: "))
+    centi = inchtoCenti(inch)
+    print("Inch: %d, Centimeters: %.2f" %(inch,centi))
