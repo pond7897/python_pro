d46db7e39ebcafcbe3421abc8aa071621905a51e
pond7897 <80212418+pond7897@users.noreply.github.com>
2024-02-24 03:16:26 +0700

    commit

---
+++ python/Homework/grader.py
@@ -0,0 +1,15 @@
+def calgrade(score):
+    if score >= 80:
+        return "A"
+    elif 70 <= score < 80:
+        return "B"
+    elif 60 <= score < 70:
+        return "C"
+    elif 50 <= score < 60:
+        return "D"
+    else:
+        return "F"
+def main():
+    score = int(input("Score: "))
+    grade = calgrade(score)
+    print(grade)
